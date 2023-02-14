903d96e180049104c6a39d0e339a005325d90016
watataku11xx <watataku131122@outlook.jp>
2023-02-15 01:47:39 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,214 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+(() => {
+  const todos = [];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  function TodoHeader(props){
+    const remaining = props.todos.filter(todo => {
+        return !todo.isDone;
+    });
+    return(
+      <h1>
+          <button onClick={props.purge}>Purge</button>
+          <button onClick={props.eraseAll}>Erase all</button>
+          My todos
+          <span>({remaining.length}/{props.todos.length})</span>
+      </h1>
+    );
+  }
+
+  function TodoItem(props){
+    return(
+      <li>
+          <label>
+              <input type="checkbox" 
+                  checked={props.todo.isDone}
+                  onChange={() => props.checkTodo(props.todo)}
+              />
+              <span className={props.todo.isDone ? 'done' : ''}>
+                  {props.todo.title}
+              </span>
+          </label>
+          <span className="cmd" onClick={() =>
+          props.deleteTodo(props.todo)}>[x]</span>
+      </li>
+    );
+  }
+
+  function TodoList(props){
+
+    const todos = props.todos.map(todo => {
+        return (
+            <TodoItem
+                key={todo.id}
+                todo={todo}
+                checkTodo={props.checkTodo}
+                deleteTodo={props.deleteTodo}
+            />
+        );
+    });
+
+    return(
+        <ul>
+            {props.todos.length ? todos : <li>Nothing todo!</li>}
+        </ul>
+    );
+  }
+
+  function TodoForm(props){
+    return(
+        <form onSubmit={props.addTodo}>
+            <input type="text" value={props.item} onChange={props.updateItem}/>
+            <input type="submit" value="add"/>
+        </form>
+    );
+
+  }
+
+  function getUniqueId(){
+    return new Date().getTime().toString(36) + '-' + Math.random().toString(36);
+  }
+
+  class App extends React.Component{
+
+    constructor(){
+        super();
+        this.state={
+            todos: todos,
+            item: ''
+        };
+
+        this.checkTodo = this.checkTodo.bind(this);
+        this.deleteTodo = this.deleteTodo.bind(this);
+        this.updateItem = this.updateItem.bind(this);
+        this.addTodo = this.addTodo.bind(this);
+        this.purge = this.purge.bind(this);
+        this.eraseAll = this.eraseAll.bind(this);
+    }
+
+    eraseAll(){
+        // if(!confirm('are you sure?')){
+        //     return;
+        // }
+        let todos = this.state.todos.splice();
+        todos.length = 0;
+        this.setState({
+            todos: todos
+        });
+    }
+
+    purge(){
+        // if(!confirm('are you sure?')){
+        //     return;
+        // }
+
+        const todos = this.state.todos.filter(todo => {
+            return !todo.isDone;
+        });
+
+        this.setState({
+        todos: todos
+        });
+    }
+
+    addTodo(e){
+        e.preventDefault();
+
+        if(this.state.item.trim() === ''){
+            return;
+        }
+
+        const item = {
+            id: getUniqueId(),
+            title: this.state.item,
+            isDone: false,
+        };
+        
+        const todos = this.state.todos.slice();
+        todos.push(item);
+        this.setState({
+            todos: todos,
+            item: '',
+        });
+    }
+
+
+    deleteTodo(todo){
+        // if(!confirm('are you sure?')){
+        //     return;
+        // }
+        const todos = this.state.todos.slice();
+        const pos = this.state.todos.indexOf(todo);
+        
+        todos.splice(pos, 1);
+        this.setState({
+            todos: todos
+        });
+    }
+
+    checkTodo(todo){
+        const todos = this.state.todos.map(todo => {
+            return {id: todo.id, title: todo.title, isDone: todo.isDone};
+        });
+
+        const pos = this.state.todos.map(todo => {
+            return todo.id
+        }).indexOf(todo.id);
+
+        todos[pos].isDone = !todos[pos].isDone;
+        this.setState({
+            todos: todos
+        });
+    }
+
+    updateItem(e){
+        this.setState({
+            item: e.target.value
+        });
+    }
+
+    componentDidUpdate(){
+        localStorage.setItem('todos', JSON.stringify(this.state.todos));
+    }
+
+    componentDidMount(){
+        this.setState({
+            todos: JSON.parse(localStorage.getItem('todos')) || []
+        });
+    }
+
+    render(){
+        return (
+
+            <div className="container">
+                <TodoHeader
+                    todos={this.state.todos}
+                    purge={this.purge}
+                    eraseAll={this.eraseAll}
+                />
+                <TodoList 
+                    todos={this.state.todos}
+                    checkTodo={this.checkTodo}
+                    deleteTodo={this.deleteTodo}
+                />
+                <TodoForm
+                    item={this.state.item}
+                    updateItem={this.updateItem}
+                    addItem={this.addItem}
+                    addTodo={this.addTodo}
+                />
+            </div>
+        );
+    }
+  }
+
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+      document.getElementById('root')
+  );
+})();
